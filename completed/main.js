b9cf2eed6f6c56c1344a8d4f3ef6bb3f15d600c3
shreyanshraj <shreyanshraj@gmail.com>
2024-04-26 18:48:22 -0700

    data processing and select options

--- completed/main.js
+++ completed/main.js
@@ -1,3 +1,6 @@
+var cat_array=['violent_crime','homicide','rape_revised','robbery','aggravated_assault','property_crime','burglary','larceny','motor_vehicle_theft']
+
+
 document.addEventListener("DOMContentLoaded",function(){
     Promise.all([
         d3.csv('/dataset/Minimum Wage Data.csv'),
@@ -6,16 +9,55 @@ document.addEventListener("DOMContentLoaded",function(){
         d3.csv('/dataset/estimated_crimes_1979_2019.csv')
 ]).then(function(values){
         
-    min_wage_data = values[0];
+    
     unemploy_data = values[1];
     crime_data = values[3];
     us_state = values[2];
 
-    console.log('wage',min_wage_data);
-    console.log('unem',unemploy_data);
-    console.log('crime',crime_data);
-    console.log('us',us_state);
+    crime_data.map(function(f){
+        f['aggravated_assault'] = +f['aggravated_assault']
+        f['violent_crime'] = +f['violent_crime']
+        f['rape_revised'] = +f['rape_revised']
+        f['homicide'] = +f['homicide']
+        f['robbery'] = +f['robbery']
+        f['property_crime'] = +f['property_crime']
+        f['burglary'] = +f['burglary']
+        f['larceny'] = +f['larceny']
+        f['motor_vehicle_theft'] = +f['motor_vehicle_theft']
+        f['year'] = +f['year']
+    })
+
+    unemploy_data.map(function(u){
+        u['Year']= + u['Year']
+        u['unemployed percent'] =  +u['unemployed percent']
+    })
+
+    // console.log('unem',unemploy_data);
+    // console.log('crime',crime_data);
+    // console.log('us',us_state);
+
+    cat_option = document.getElementById("cat-dd");
+        // cat_option.innerHTML=""
+    for (var i=0; i< cat_array.length; i++){
+        header = cat_array[i]
+        var el = document.createElement("option");
+        el.textContent = header;
+        el.value=  header;
+        cat_option.appendChild(el)
+    }
 
 
+    // dataFilter();
+    // mapPlot()
     })
 })
+
+function dataFilter(){
+    
+    
+}
+
+
+function mapPlot(){
+
+}
